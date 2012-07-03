cc3fbd9c4f88248bc65e1709133d47afc45a3521
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 00:41:22 +0300

    Added home controller.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+# Courseware Main Page Controller class
+class HomeController < ApplicationController
+  before_filter :require_login, {only: :index}
+
+  # Main page handler
+  def index
+  end
+end
