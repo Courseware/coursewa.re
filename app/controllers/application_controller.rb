c3c1742217d47adb456f50e759bf8f3ec54d9254
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 00:51:56 +0300

    Overwrote sorcery `not_authenticated` method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # Courseware application controller
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+    # Sorcery method overwritten to customize error message
+    def not_authenticated
+      redirect_to login_url, alert: 'First log in to view this page.'
+    end
 end
