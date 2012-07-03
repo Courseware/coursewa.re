5b1f668b541be2515dacfecede958581bf8652db
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 00:50:41 +0300

    Added routes for user and session controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Courseware::Application.routes.draw do
+  get 'logout' => 'sessions#destroy', :as => 'logout'
+  get 'login' => 'sessions#new', :as => 'login'
+  get 'signup' => 'users#new', :as => 'signup'
+
+  resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
+
+  root :to => 'home#index'
 end
