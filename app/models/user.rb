ec3fef53eb6356587be760cad1c9c6a75a8efc9f
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 00:11:38 +0300

    Add some validations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 # Courseware User model
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  attr_accessible :email, :password, :password_confirmation
+
+  # Validations
+  validates_confirmation_of :password
+  validates_presence_of :password, { on: :create }
+  validates_presence_of :email
+  validates_uniqueness_of :email
 end
