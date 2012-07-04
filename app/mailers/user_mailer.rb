59eac0d9d4eefbffe6a326d2c7f65627755705af
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 15:37:10 +0300

    Cleanup, add reset password mailer method.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,10 +10,10 @@ class UserMailer < ActionMailer::Base
   #
   # @param user {User}, to send email to
   def activation_needed_email(user)
-    # @user = user
-    # @url  = "http://0.0.0.0:3000/users/#{user.activation_token}/activate"
-    # mail(to: user.email,
-    #      subject: "Welcome to My Awesome Site")
+    @user = user
+    @url  = "http://0.0.0.0:3000/users/#{user.activation_token}/activate"
+    mail(to: user.email,
+         subject: "Welcome to My Awesome Site")
   end
 
   # Sends an activation confirmation email to the user
@@ -29,4 +29,19 @@ class UserMailer < ActionMailer::Base
     mail(to: user.email,
          subject: "Your account is now activated")
   end
+
+  # Sends a reset password email to the user
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset_password_email.subject
+  #
+  # @param user {User}, to send email to
+  def reset_password_email(user)
+    @user = user
+    @url  = "http://0.0.0.0:3000/login"
+    mail(to: user.email,
+         subject: "Your password has been reset")
+  end
+
 end
