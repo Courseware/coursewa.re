d4972338c337a1963ed412b3a211ece2ea77e270
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 23:23:07 +0300

    Cleanups.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,10 @@
 # Courseware Main Page Controller class
 class HomeController < ApplicationController
+
   before_filter :require_login, {only: :index}
 
   # Main page handler
   def index
   end
+
 end
