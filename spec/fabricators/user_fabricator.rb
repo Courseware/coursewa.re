8aec2c77f85f3fbbddc200cb98e1fc98a658fb2a
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 15:37:59 +0300

    User fabricator, fix a typo, add more attributes.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,4 +1,7 @@
 Fabricator(:user) do
   username  { Faker::Internet.user_name }
-  email     { |attrs| "#{attrs[:user_name]}@#{Courseware.config.domain_name}" }
+  email     { |attrs| "#{attrs[:username]}@#{Courseware.config.domain_name}" }
+  first_name { Faker::Name.first_name }
+  last_name { Faker::Name.last_name }
+  password  { 'secret' }
 end
