825ed8e36ef5c9808337b89b8f2291144ee01461
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-04 18:18:31 +0300

    Added courseware text domain.

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,6 +1,6 @@
-FastGettext.add_text_domain 'app', :path => 'locale'
 FastGettext.default_available_locales = ['en','ro']
 FastGettext.default_text_domain = 'courseware'
+FastGettext.add_text_domain 'courseware', :path => 'locale'
 
 # Trust all usages of '%' in translations
 GettextI18nRails.translations_are_html_safe = true
