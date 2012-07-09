bf206ec4381ff5f04d6957de6d5d72d73f7ad4de
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-09 18:13:51 +0300

    Added user model validations for username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ActiveRecord::Base
   # Validations
   validates_confirmation_of :password
   validates_presence_of :password, { on: :create }
+
+  validates_presence_of :username
   validates_presence_of :email
+
+  validates_uniqueness_of :username
   validates_uniqueness_of :email
 end
