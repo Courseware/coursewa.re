d38a1646b0ff039a3212e2905e0444837b7e6902
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-09 18:21:29 +0300

    Do not load custom forms jquery plugin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
 // require_tree .
 
 // =require foundation/modernizr.foundation
-// =require foundation/jquery.customforms
+// require foundation/jquery.customforms
 // require foundation/jquery.orbit-1.4.0
 // =require foundation/jquery.placeholder.min
 // =require foundation/jquery.reveal
