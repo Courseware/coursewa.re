b5f7156fac44c7f1fbaa0776fabdb47e4bdf9fc7
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-10 21:52:32 +0300

    Updated user fabricator to strip chars like `.` and `_`.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,5 +1,6 @@
 Fabricator(:user) do
-  username  { Faker::Internet.user_name }
+  # Remove special chars to help faker a bit with the username
+  username  { Faker::Internet.user_name.gsub(/[\.\_]/, '-') }
   email     { |attrs| "#{attrs[:username]}@#{Courseware.config.domain_name}" }
   first_name { Faker::Name.first_name }
   last_name { Faker::Name.last_name }
