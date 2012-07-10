f1e712fa391f54d8c6f7814ecba6be4b669f0a2d
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-10 21:51:44 +0300

    Added more validations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,15 @@ class User < ActiveRecord::Base
   # Validations
   validates_confirmation_of :password
   validates_presence_of :password, { on: :create }
+  validates_length_of :password, :minimum => 6, :maximum => 32
 
+  validates_exclusion_of :username, :in => Courseware.config.domain_blacklist
+  validates_uniqueness_of :username, :case_sensitive => false
+  validates_format_of :username,:with => /^[a-z0-9\-]+$/
+  validates_length_of :username, :minimum => 1, :maximum => 32
   validates_presence_of :username
-  validates_presence_of :email
 
-  validates_uniqueness_of :username
+  validates_presence_of :email
   validates_uniqueness_of :email
 
   # Helper to generate user's name
