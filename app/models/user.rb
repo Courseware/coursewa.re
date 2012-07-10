1cdc9da0c9166b34fad9f8547537b5aac7648cdd
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-10 21:50:46 +0300

    Added `User#name` method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :username
   validates_uniqueness_of :email
+
+  # Helper to generate user's name
+  def name
+    return '%s %s' % [first_name, last_name] if first_name and last_name
+    username
+  end
+
 end
