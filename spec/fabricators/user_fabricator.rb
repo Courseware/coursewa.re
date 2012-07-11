3474cc77a7c718111d5cfd7ad74895c2b0bc9953
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-11 19:16:33 +0300

    Cleaned up user fabricator.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:user) do
-  email       { Faker::Internet.email }
-  first_name  { Faker::Name.first_name }
-  last_name   { Faker::Name.last_name }
-  password    { 'secret' }
+  email       Faker::Internet.email
+  first_name  Faker::Name.first_name
+  last_name   Faker::Name.last_name
+  password    'secret'
 end
