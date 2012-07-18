e8e38ef2b51787685b689718e6dadd15810c2db2
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-18 20:44:38 +0300

    Add a spec to test subdomain routing to group.

--- spec/routing/home_routing_spec.rb
+++ spec/routing/home_routing_spec.rb
@@ -7,5 +7,11 @@ describe HomeController do
       get('/').should route_to('home#index')
     end
 
+    describe 'on a subdomain' do
+      it 'goes to group langing page' do
+        get('http://test.coursewa.re').should route_to('groups#show')
+      end
+    end
+
   end
 end
