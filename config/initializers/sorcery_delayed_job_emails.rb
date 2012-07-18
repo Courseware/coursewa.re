f0dc42d9edd65e100118ab5c180abe82ff34ce75
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-18 23:09:35 +0300

    Cleanup a bit sorcery delayed job integration. Enable it if only dj is on.
    This way `ActionMailer::Base#deliveries` should not be affected.

--- config/initializers/sorcery_delayed_job_emails.rb
+++ config/initializers/sorcery_delayed_job_emails.rb
@@ -1,11 +1,10 @@
-module Sorcery
-  module Model
-    module InstanceMethods
-      def generic_send_email(method, mailer)
-        config = sorcery_config
-        mail = config.send(mailer).delay.send(config.send(method), self)
-      end
-    end
+module Sorcery::Model::InstanceMethods
+  alias_method(
+    :async_send_email, :generic_send_email
+  ) if Delayed::Worker.delay_jobs
+
+  def async_send_email(method, mailer)
+    config = sorcery_config
+    mail = config.send(mailer).delay.send(config.send(method), self)
   end
 end
-
