453ca45f67c629d19da45bb7cce997da92448470
Stas SUȘCOV <stas@net.utcluj.ro>
2012-07-20 22:56:04 +0300

    Updated user fabricator to allow sequences of users.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:user) do
-  email       Faker::Internet.email
+  email       { sequence(:email) { Faker::Internet.email } }
   first_name  Faker::Name.first_name
   last_name   Faker::Name.last_name
   password    'secret'
