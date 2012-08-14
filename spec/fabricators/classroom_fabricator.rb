9a8f5b291d7f8959991bc4d15a14bd41e0feb0d9
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-15 01:25:37 +0300

    Classroom fabricator should activate owner account.

--- spec/fabricators/classroom_fabricator.rb
+++ spec/fabricators/classroom_fabricator.rb
@@ -2,4 +2,6 @@ Fabricator(:classroom) do
   title       Faker::Education.school[0..31]
   description Faker::Lorem.sentence
   owner(:fabricator => :user)
+
+  after_build { |classroom| classroom.owner.activate! }
 end
