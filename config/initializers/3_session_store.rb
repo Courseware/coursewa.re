4dff7670de15ed598deb50df46860958719d6140
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-15 01:24:10 +0300

    Share session cookie across all the domains.

--- config/initializers/3_session_store.rb
+++ config/initializers/3_session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Courseware::Application.config.session_store(
-  :cookie_store, key: '_courseware_session'
+  :cookie_store, key: '_courseware_session', :domain => :all
 )
 
 # Use the database for sessions instead of the cookie-based default,
