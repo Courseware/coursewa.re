519c64d5c1939d9c9993078200bf624809f070b0
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-15 01:20:42 +0300

    Cleanup application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # Courseware application controller
 class ApplicationController < ActionController::Base
+  # CSRF protection
   protect_from_forgery
+
+  # Abilities checking
+  check_authorization
 
   # Setup locale
   before_filter :set_gettext_locale
@@ -8,9 +12,23 @@ class ApplicationController < ActionController::Base
   # Ask users to authenticate
   before_filter :require_login
 
+  # Handle errors
+  rescue_from CanCan::AccessDenied, :with => :unauthorized
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+
   private
     # Sorcery method overwritten to customize error message
     def not_authenticated
-      redirect_to login_url, :alert => _('First log in to view this page.')
+      redirect_to(login_url, :alert => _('Please authenticate first.'))
+    end
+
+    # Unauthorized error handler
+    def unauthorized
+      redirect_to(root_url, :alert => _('Access to this page is not allowed.'))
+    end
+
+    # Not found handler
+    def not_found
+      render(:status => 404)
     end
 end
