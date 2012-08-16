6b47ffe155ee72f5a5ed98dc81fe6e138cc85a1c
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-16 16:08:37 +0300

    Add subdomain log tags in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,7 @@ Courseware::Application.configure do
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = [ :subdomain ]
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
