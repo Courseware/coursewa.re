a8a5e47bed810be86ee7b35014278dfd6e6731d2
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-16 16:09:18 +0300

    Upgraded zurb foundation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,27 +1,26 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-
+/**
+ * Coursewa.re v0.1
+ */
 
 // =require ./_webfont
 
 // =require jquery
 // require jquery_ujs
-// require_tree .
 
 // =require foundation/modernizr.foundation
-// require foundation/jquery.customforms
-// require foundation/jquery.orbit-1.4.0
-// =require foundation/jquery.placeholder.min
-// =require foundation/jquery.reveal
-// require foundation/jquery.tooltips
+// =require foundation/jquery.placeholder
+// =require foundation/jquery.foundation.alerts
+// require foundation/jquery.foundation.accordion
+// =require foundation/jquery.foundation.buttons
+// require foundation/jquery.foundation.tooltips
+// require foundation/jquery.foundation.forms
+// require foundation/jquery.foundation.tabs
+// require foundation/jquery.foundation.navigation
+// =require foundation/jquery.foundation.reveal
+// require foundation/jquery.foundation.orbit
+// require foundation/jquery.foundation.mediaQueryToggle
+
+
+// require_tree .
 
 // =require ./_courseware
