858f8adc5d6c0ef7aa66f03fa9bbc20b29227ab3
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-27 16:05:48 +0300

    Fixed classroom fabricator to generate unique titles using sequence.

--- spec/fabricators/classroom_fabricator.rb
+++ spec/fabricators/classroom_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:classroom) do
-  title       Faker::Education.school[0..31]
+  title       { sequence(:class_title){ Faker::Education.school[0..31] } }
   description Faker::Lorem.sentence
   owner(:fabricator => :user)
 
