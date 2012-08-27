f8c2c3f7518849a2fe4b508a0171cbffc815c7f8
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-28 02:06:49 +0300

    Added a helper for head embeds. In our case to include editor assets.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, page_title.to_s
   end
+
+  # Helper to set embeds in HEAD
+  #
+  # @param content, ebeds content
+  def head_embed(content)
+    content_for :head_embed, content if content
+  end
 end
