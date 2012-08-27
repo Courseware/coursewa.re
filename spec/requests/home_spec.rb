88ce81005267e9d908f869161abc3633319853f9
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-28 02:10:32 +0300

    Added sidebar link to create a classroom. Unavailable if limits reached.

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe 'Home' do
-  let(:user){ Fabricate(:user) }
-
   context 'when not logged in' do
     it 'should show dasboard' do
       visit root_url
@@ -11,12 +9,25 @@ describe 'Home' do
   end
 
   context 'when logged in' do
+    let(:user){ Fabricate(:user) }
+
     it 'should show dasboard' do
       visit activate_user_url(user.activation_token)
       sign_in_with(user.email)
       visit root_url
       page.should have_css('#dashboard-home')
       page.should have_css('#activities .user-create')
+      page.should have_xpath("//a[@href='#{start_classroom_path}']")
+    end
+  end
+
+  context 'when classrooms limits reached' do
+    let(:user){ Fabricate(:classroom).owner.reload }
+
+    it 'should not show classrooms creation link' do
+      sign_in_with(user.email)
+      visit root_url
+      page.should_not have_xpath("//a[@href='#{start_classroom_path}']")
     end
   end
 
