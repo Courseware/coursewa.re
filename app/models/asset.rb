a1769a69edd49c713db7a93a244907a906484eb5
Stas SUȘCOV <stas@net.utcluj.ro>
2012-08-28 19:54:52 +0300

    Updated Plan model callbacks and specs when a user uploads/deletes files.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,4 +5,21 @@ class Asset < ActiveRecord::Base
   # Relationships
   belongs_to :assetable, :polymorphic => true
   delegate :url, :to => :attachment
+
+  # Validations
+  validates_attachment_size :attachment, :less_than => Proc.new{
+    |img| img.user.plan.left_space
+  }
+
+  # Callbacks
+
+  # Increment user used space
+  after_create do
+    user.plan.increment!(:used_space, attachment_file_size)
+  end
+
+  # Decrement freed space
+  before_destroy do
+    user.plan.decrement!(:used_space, attachment_file_size)
+  end
 end
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -9,9 +9,50 @@ describe Plan do
     it { should allow_value(plan).for(:slug) }
   end
 
-  describe 'with all attributes' do
-    subject{ Fabricate(:plan) }
-    its(:user) { should be_a(User) }
+  context 'with all attributes' do
+    subject{ plan }
+    its(:used_space){ should eq(0) }
+
+    context 'when a user uploads a file' do
+      it 'should update the used space' do
+        img = Fabricate(:image, :user => plan.user)
+        img_size = img.attachment_file_size
+
+        plan.reload
+        plan.used_space.should eq(img_size)
+        plan.left_space.should eq(plan.allowed_space - img_size)
+      end
+    end
+
+    context 'when a user deletes a file' do
+      it 'should update the used space' do
+        img = Fabricate(:image, :user => plan.user)
+        img_size = img.attachment_file_size
+        initial_used_space = plan.reload.used_space
+
+        img.destroy
+        plan.reload
+        plan.used_space.should eq(initial_used_space - img_size)
+      end
+    end
+
+    context 'when a user reached plan limits' do
+      before{ plan.update_attribute(:allowed_space, 0) }
+
+      it 'should fail uploading new files' do
+        plan.reload
+        plan.allowed_space.should eq(0)
+
+        initial_used_space = plan.reload.used_space
+        expect{
+          Fabricate(:image, :user => plan.user)
+        }.to raise_error(ActiveRecord::RecordInvalid, /Attachment file size/)
+
+        plan.reload
+        plan.used_space.should eq(initial_used_space)
+      end
+    end
+
   end
 
 end
