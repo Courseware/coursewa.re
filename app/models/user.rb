6e8286d596f8041ef3d81d60ac222d634094ca8b
Stas SUȘCOV <stas@net.utcluj.ro>
2012-09-19 18:13:45 +0300

    Updated user model to allow updating first_name and last_name fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
 
   authenticates_with_sorcery!
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation,
+    :first_name, :last_name
 
   # Relationships
   has_many(
