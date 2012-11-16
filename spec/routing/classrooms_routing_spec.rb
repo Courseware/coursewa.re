4f2130e8b47656c0345d12353a9fdfb6c60fe1c3
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-16 17:46:45 +0200

    Updated classroom routing spec.

--- spec/routing/classrooms_routing_spec.rb
+++ spec/routing/classrooms_routing_spec.rb
@@ -3,8 +3,16 @@ require 'spec_helper'
 describe ClassroomsController do
   describe 'routing' do
 
-    it 'for classroom creation scree' do
+    it 'for creation screen' do
       get('/start').should route_to('classrooms#new')
+    end
+
+    it 'for creation' do
+      post('/start').should route_to('classrooms#create')
+    end
+
+    it 'for dashboard screen' do
+      get('http://test.lvh.me/').should route_to('classrooms#dashboard')
     end
 
   end
