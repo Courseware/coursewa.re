ff1c2b572f3ebf0894c902bdeabfcdb41192a005
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-18 11:34:01 +0200

    Updated syllabus requests spec for update and create actions.

--- spec/requests/syllabuses_spec.rb
+++ spec/requests/syllabuses_spec.rb
@@ -8,11 +8,32 @@ describe 'Syllabuses' do
     page.should have_css('#login')
   end
 
+  it 'it should redirect without update if not logged in' do
+    visit edit_syllabus_url(:subdomain => classroom.slug)
+    page.should have_css('#login')
+  end
+
   it 'it should show syllabus missing if logged in' do
     sign_in_with(classroom.owner.email)
     visit syllabus_url(:subdomain => classroom.slug)
     page.should have_css('#syllabus')
     page.should have_css('#syllabus .syllabus-create')
+  end
+
+  it 'it should create syllabus if logged in' do
+    sign_in_with(classroom.owner.email)
+    visit edit_syllabus_url(:subdomain => classroom.slug)
+
+    syllabus = Fabricate.build(
+      :syllabus, :classroom => classroom, :user => classroom.owner)
+
+    page.fill_in('syllabus_title', :with => syllabus.title)
+    page.fill_in('syllabus_intro', :with => syllabus.intro)
+    page.fill_in('syllabus_content', :with => syllabus.content)
+    page.click_on('submit_syllabus')
+
+    page.should have_content(syllabus.title)
+    page.should have_css('#notifications .success')
   end
 
   describe 'when syllabus exists' do
@@ -32,7 +53,7 @@ describe 'Syllabuses' do
       page.should_not have_css('#syllabus .syllabus-create')
     end
 
-    it 'it should update syllabus if logged in' do
+    it 'it should show update syllabus if logged in' do
       sign_in_with(classroom.owner.email)
       visit edit_syllabus_url(:subdomain => classroom.slug)
 
@@ -40,6 +61,22 @@ describe 'Syllabuses' do
       page.should have_css('#syllabus_intro')
       page.should have_css('#syllabus_content')
       page.should have_css('#submit_syllabus')
+    end
+
+    it 'it should update syllabus if logged in' do
+      sign_in_with(classroom.owner.email)
+      visit edit_syllabus_url(:subdomain => classroom.slug)
+
+      syllabus = Fabricate.build(
+        :syllabus, :classroom => classroom, :user => classroom.owner)
+
+      page.fill_in('syllabus_title', :with => syllabus.title)
+      page.fill_in('syllabus_intro', :with => syllabus.intro)
+      page.fill_in('syllabus_content', :with => syllabus.content)
+      page.click_on('submit_syllabus')
+
+      page.should have_content(syllabus.title)
+      page.should have_css('#notifications .success')
     end
   end
 
