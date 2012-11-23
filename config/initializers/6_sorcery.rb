43299daa912fe009f21070af7948af694283fec8
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-23 03:41:13 +0200

    Update sorcery user model.

--- config/initializers/6_sorcery.rb
+++ config/initializers/6_sorcery.rb
@@ -434,5 +434,5 @@ Rails.application.config.sorcery.configure do |config|
 
   # This line must come after the 'user config' block.
   # Define which model authenticates with sorcery.
-  config.user_class = 'User'
+  config.user_class = 'Coursewareable::User'
 end
