b22638678444a53e568c23387dca17645c298ce4
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-28 21:20:32 +0200

    Cleanup courseware config.

--- config/initializers/8_courseware.rb
+++ config/initializers/8_courseware.rb
@@ -1,48 +1,47 @@
 require 'ostruct'
+require 'coursewareable'
 
 # Courseware configuration entries
 module Courseware
-  class << self
-    attr_reader :config
-  end
-
-  @config = OpenStruct.new(
-    :domain_name => 'coursewa.re',
-    :default_email_address => 'no-reply@coursewa.re',
-    :domain_blacklist => %w(blog api support help mail ftp dashboard),
-    :plans => {
-      :free => {
-        :slug => :free,
-        :allowed_classrooms => 1,
-        :allowed_collaborators => 0,
-        :allowed_space => 100.megabytes,
-        :expires_in => nil,
-        :cost => 0
-      },
-      :micro => {
-        :slug => :micro,
-        :allowed_classrooms => 5,
-        :allowed_collaborators => 5,
-        :allowed_space => 5.gigabytes,
-        :expires_in => Time.now + 1.month,
-        :cost => 7
-      },
-      :small => {
-        :slug => :small,
-        :allowed_classrooms => 10,
-        :allowed_collaborators => 10,
-        :allowed_space => 10.gigabytes,
-        :expires_in => Time.now + 1.month,
-        :cost => 12
-      },
-      :medium => {
-        :slug => :medium,
-        :allowed_classrooms => 20,
-        :allowed_collaborators => 20,
-        :allowed_space => 20.gigabytes,
-        :expires_in => Time.now + 1.month,
-        :cost => 22
-      }
-    }
-  )
+  mattr_accessor :config
 end
+
+Courseware.config = Coursewareable.config = OpenStruct.new(
+  :domain_name => 'coursewa.re',
+  :default_email_address => 'no-reply@coursewa.re',
+  :domain_blacklist => %w(blog api support help mail ftp dashboard),
+  :plans => {
+    :free => {
+      :slug => :free,
+      :allowed_classrooms => 1,
+      :allowed_collaborators => 0,
+      :allowed_space => 100.megabytes,
+      :expires_in => nil,
+      :cost => 0
+    },
+    :micro => {
+      :slug => :micro,
+      :allowed_classrooms => 5,
+      :allowed_collaborators => 5,
+      :allowed_space => 5.gigabytes,
+      :expires_in => Time.now + 1.month,
+      :cost => 7
+    },
+    :small => {
+      :slug => :small,
+      :allowed_classrooms => 10,
+      :allowed_collaborators => 10,
+      :allowed_space => 10.gigabytes,
+      :expires_in => Time.now + 1.month,
+      :cost => 12
+    },
+    :medium => {
+      :slug => :medium,
+      :allowed_classrooms => 20,
+      :allowed_collaborators => 20,
+      :allowed_space => 20.gigabytes,
+      :expires_in => Time.now + 1.month,
+      :cost => 22
+    }
+  }
+)
