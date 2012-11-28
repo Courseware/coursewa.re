ccf78804c4d51b41d51d27e748f028e390635b24
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-28 21:21:02 +0200

    Update application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 # Courseware application controller
-class ApplicationController < Coursewareable::ApplicationController
+class ApplicationController < ActionController::Base
+  # CSRF protection
+  protect_from_forgery
+
+  # Setup locale
+  before_filter :set_gettext_locale
 end
--- app/controllers/coursewareable/application_controller.rb
+++ app/controllers/coursewareable/application_controller.rb
@@ -1,16 +1,6 @@
 module Coursewareable
   # Courseware application controller
-  class ApplicationController < ActionController::Base
-
-    # CSRF protection
-    protect_from_forgery
-
-    # Abilities checking
-    load_and_authorize_resource
-
-    # Setup locale
-    before_filter :set_gettext_locale
-
+  class ApplicationController < ::ApplicationController
     # Ask users to authenticate
     before_filter :require_login
 
@@ -19,6 +9,7 @@ module Coursewareable
     rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 
     private
+
     # Sorcery method overwritten to customize error message
     def not_authenticated
       redirect_to(login_url, :alert => _('Please authenticate first.'))
