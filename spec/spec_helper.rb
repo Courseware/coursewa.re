83fee8600b92ceba42f79882c6c14a5e33cbcae5
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-28 21:18:01 +0200

    Move capybara into a separated support file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,6 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'shoulda-matchers'
-  require 'capybara/rspec'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
