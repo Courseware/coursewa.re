99452b6211a807e74e6ce0e4bb07bd901fa8ad44
Stas SUȘCOV <stas@net.utcluj.ro>
2012-12-07 17:10:10 +0200

    Added lectures routing specs.

---
+++ spec/routing/coursewareable/lectures_routing_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Coursewareable::LecturesController do
+
+  before do
+    @routes = Coursewareable::Engine.routes
+  end
+
+  describe 'routing' do
+    it 'for new lecture' do
+      get('http://test.lvh.me/lectures/new').should route_to(
+        'coursewareable/lectures#new')
+    end
+
+    it 'for lecture' do
+      get('http://test.lvh.me/lectures/test').should route_to(
+        'coursewareable/lectures#show', :id => 'test')
+    end
+
+    it 'for creation screen' do
+      get('http://test.lvh.me/lectures/test/edit').should route_to(
+        'coursewareable/lectures#edit', :id => 'test')
+    end
+
+    it 'for creation' do
+      post('http://test.lvh.me/lectures').should route_to(
+        'coursewareable/lectures#create')
+    end
+
+    it 'for updating' do
+      put('http://test.lvh.me/lectures/test').should route_to(
+        'coursewareable/lectures#update', :id => 'test')
+    end
+
+    it 'for deletion' do
+      delete('http://test.lvh.me/lectures/test').should route_to(
+        'coursewareable/lectures#destroy', :id => 'test')
+    end
+  end
+end
