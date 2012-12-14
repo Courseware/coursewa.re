9f442c1b42f3f142f0e6f16a95249a98ef81bc09
Stas SUȘCOV <stas@net.utcluj.ro>
2012-12-14 19:29:25 +0200

    Updated assignments controller with proper logic.

--- app/controllers/coursewareable/assignments_controller.rb
+++ app/controllers/coursewareable/assignments_controller.rb
@@ -1,22 +1,76 @@
 module Coursewareable
   # Assignments controller
   class AssignmentsController < ApplicationController
+
+    # Abilities checking for our nested resource
+    load_and_authorize_resource :class => Coursewareable::Assignment
+    skip_authorize_resource :only => [:create, :new]
+
+    before_filter :load_classroom_lecture
+
+    # Handles creation screen
     def new
+      @assignment = @lecture.assignments.build(params[:assignment])
+      @assignment.user = current_user
+
+      authorize!(:create, @assignment)
     end
 
+    # Handles creation
     def create
+      @assignment = @lecture.assignments.build(params[:assignment])
+      @assignment.user = current_user
+
+      authorize!(:create, @assignment)
+
+      if @assignment.save
+        flash[:success] = _('Assignment was created.')
+        redirect_to edit_lecture_assignment_url(@lecture, @assignment)
+      else
+        flash[:alert] = _('There was an error, please try again.')
+        render :new
+      end
     end
 
+    # Handles assignment screen
     def show
     end
 
+    # Handles editing screen
     def edit
     end
 
+    # Handles update
     def update
+      @assignment = @lecture.assignments.find(params[:id])
+      @assignments.update_attributes(params[:assignment])
+
+      if @assignment.save
+        flash[:success] = _('Assignment was updated.')
+      else
+        flash[:alert] = _('There was an error, please try again.')
+      end
+
+      redirect_to edit_lecture_assignment_url(@lecture, @assignment)
     end
 
+    # Handles removal
     def destroy
+      assignment = @lecture.assignments.find(params[:id])
+
+      if assignment.destroy
+        flash[:success] = _('Lecture removed.')
+      end
+
+      redirect_to lecture_url(@lecture)
+    end
+
+    protected
+
+    # Loads current classroom and lecture
+    def load_classroom_lecture
+      @classroom = Coursewareable::Classroom.find_by_slug!(request.subdomain)
+      @lecture = @Classroom.lectures.find_by_slug(params[:lecture_id])
     end
   end
 end
