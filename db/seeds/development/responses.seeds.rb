bacba7d60926b1f9ee692b55f2b164e57f8cf3ff
Stas SUȘCOV <stas@net.utcluj.ro>
2013-01-04 05:02:07 +0200

    Added responses seed.

---
+++ db/seeds/development/responses.seeds.rb
@@ -0,0 +1,28 @@
+after 'development:assignments', 'development:memberships' do
+  me = Coursewareable::User.find_by_email('stas@nerd.ro')
+  my_classroom = me.classrooms.first
+  members = my_classroom.members - [me]
+
+  assignment = my_classroom.assignments.offset(2).last
+  Fabricate('coursewareable/response', :classroom => my_classroom,
+            :user => members.first, :assignment => assignment) do
+
+    answers { assignment.quiz }
+  end
+
+  assignment = my_classroom.assignments.offset(2).first
+  Fabricate('coursewareable/response', :classroom => my_classroom,
+            :user => members.last, :assignment => assignment) do
+    answers {
+      answ = assignment.quiz
+      answ[0]['options'][0]['content'] = 'WRONG ANSWER'
+      answ[0]['options'][0]['wrong'] = true
+
+      answ[1]['options'][0]['wrong'] = true
+
+      answ[2]['options'][0]['wrong'] = true
+      answ
+    }
+  end
+
+end
