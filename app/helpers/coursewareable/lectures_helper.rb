af85efdacc7c5a7e64295653335a2c162871c543
Stas SUȘCOV <stas@net.utcluj.ro>
2013-01-18 18:01:21 +0200

    Add the selected option to the lectures options list helper.

--- app/helpers/coursewareable/lectures_helper.rb
+++ app/helpers/coursewareable/lectures_helper.rb
@@ -2,13 +2,14 @@ module Coursewareable
   # Courseware lectures controller helpers
   module LecturesHelper
     # Outputs a hierarchical structure of lectures based on its parent/children
-    def list_lecture_options(lectures, listed_ids = [], indent = '')
+    def list_lecture_options(lectures, selected, listed_ids = [], indent = '')
       output = ''
       lectures.each do |lecture|
         unless listed_ids.include?(lecture.id)
           listed_ids.push(lecture.id)
           output += render(:partial => 'lecture_options', :locals => {
-            :lecture => lecture, :listed_ids => listed_ids, :indent => indent})
+            :lecture => lecture, :listed_ids => listed_ids, :indent => indent,
+            :selected => selected})
         end
       end
       return output.html_safe
