62df42654427b9a07791e95ae63fdee8d794566d
Stas SUȘCOV <stas@net.utcluj.ro>
2013-01-21 22:56:12 +0200

    Added default classroom header image.
    Added application helper specs.

--- app/helpers/coursewareable/application_helper.rb
+++ app/helpers/coursewareable/application_helper.rb
@@ -14,5 +14,34 @@ module Coursewareable
     def head_embed(content)
       content_for :head_embed, content if content
     end
+
+    # Helper to render the classroom header image
+    # Defaults to a randomly provided image
+    #
+    # @return [String], generated style tag with CSS
+    def header_image
+      return if @classroom.nil?
+
+      image = @classroom.images.find_by_id(@classroom.header_image)
+      return if image.nil?
+
+      @header_image = image.attachment.url
+      render('shared/header_image')
+    end
+
+    # Generates a random header image from available list
+    # Uses last classroom digit from it's ID
+    #
+    # @return [String], generated style tag with CSS
+    def default_header_image
+      return if @classroom.nil?
+
+      image_id = @classroom.id % 10
+
+      # Since we have only 1..7 available headers, lets map what doesn't fit
+      image_id = 7 if image_id == 0 or image_id > 7
+      @header_image = path_to_image("headers/#{image_id}.jpg")
+      render('shared/header_image')
+    end
   end
 end
---
+++ spec/helpers/coursewareable/application_helper_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe Coursewareable::ApplicationHelper do
+  describe '#title' do
+    let(:title_text) { Faker::Lorem.sentence }
+    before(:each) { @view_flow = ActionView::OutputFlow.new }
+
+    subject { helper.content_for(:title) }
+
+    context 'when no title was set' do
+      it { should be_empty }
+    end
+
+    context 'when title was set' do
+      before { title(title_text) }
+
+      it { should match(title_text) }
+    end
+  end
+
+  describe '#head_embed' do
+    let(:content_text) { Faker::Lorem.sentence }
+
+    before(:each) { @view_flow = ActionView::OutputFlow.new }
+
+    subject { helper.content_for(:head_embed) }
+
+    context 'when no head_embed was set' do
+      it { should be_empty }
+    end
+
+    context 'when head_embed was set' do
+      before { head_embed(content_text) }
+
+      it { should match(content_text) }
+    end
+  end
+
+  describe '#header_image' do
+    let(:image) { Fabricate('coursewareable/image') }
+    let(:classroom) { image.classroom }
+
+    subject { helper.header_image }
+
+    context 'missing @classroom object' do
+      it { should be_nil }
+    end
+
+    context 'with a valid @classroom object' do
+      before do
+        classroom.header_image = image.id
+        assign(:classroom, classroom)
+      end
+
+      it { should match(image.attachment_file_name) }
+    end
+  end
+
+  describe '#default_header_image' do
+    let(:classroom) { Fabricate.build('coursewareable/classroom') }
+
+    subject { helper.default_header_image }
+
+    context 'missing @classroom object' do
+      it { should be_nil }
+    end
+
+    context 'with a valid @classroom object' do
+      before do
+        classroom.id = 3
+        assign(:classroom, classroom)
+      end
+
+      it { should match('assets/headers/3.jpg') }
+    end
+  end
+end
