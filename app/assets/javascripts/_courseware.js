452fc58431385faa85a71f2325e1333a88c9fea8
Stas SUȘCOV <stas@net.utcluj.ro>
2013-01-24 00:56:25 +0200

    Adapt kaminari to run xhr requests on next page clicks.

--- app/assets/javascripts/_courseware.js
+++ app/assets/javascripts/_courseware.js
@@ -6,9 +6,30 @@
 
   win.Courseware = {
     /**
+     * Handles xhr "tagged" requests
+     * On `trigger` clicks element `data="parent-container-id"`
+     * element will be replaced with ajax result.
+     */
+    enable_xhr_requests: function(selector, trigger) {
+      $(selector).on('click', trigger, function(event){
+        var href = $(this).attr('href'),
+          target = '#' + $(this).data('parent-container-id');
+
+        $.ajax({
+          url: href,
+          success: function(content) {
+            $(target).replaceWith(content)
+          }
+        })
+        return false;
+      });
+    },
+    /**
      * Start Courseware plugins/modules
      */
     run: function() {
+      Courseware.enable_xhr_requests('#content', 'a.run-as-xhr');
+
       $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
       $.fn.foundationButtons          ? $doc.foundationButtons() : null;
       $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
