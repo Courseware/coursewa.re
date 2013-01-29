8a0ce38e2886aa17a91aedad90c638027a25c1c5
Stas SUȘCOV <stas@net.utcluj.ro>
2013-01-29 19:19:47 +0200

    Hide plan start date, show expiration date if any. @wip

--- config/initializers/10_time_formats.rb
+++ config/initializers/10_time_formats.rb
@@ -1,6 +1,7 @@
 Time::DATE_FORMATS[:month_and_year] = '%B %Y'
 Time::DATE_FORMATS[:pretty_date] = '%a, %b %e'
 Time::DATE_FORMATS[:pretty_time] = '%H:%M'
+Time::DATE_FORMATS[:pretty_date_and_year] = '%a, %b %e, %Y'
 Time::DATE_FORMATS[:pretty] = lambda { |time|
   _('%s at %s') % [time.strftime('%a, %b %e'), time.strftime('%H:%M')]
 }
