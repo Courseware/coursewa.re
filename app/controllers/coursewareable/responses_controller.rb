6a29fe34c3f3506228cd64cf19f71637a146c853
Stas SUȘCOV <stas@net.utcluj.ro>
2013-02-18 23:21:31 +0200

    Current lecture missing on creation. Closes #24

--- app/controllers/coursewareable/responses_controller.rb
+++ app/controllers/coursewareable/responses_controller.rb
@@ -20,6 +20,7 @@ module Coursewareable
 
     # Handles response creation
     def create
+      @lecture = @assignment.lecture
       @response = @assignment.responses.build(params[:response])
       @response.classroom = @classroom
       @response.user = current_user
@@ -31,7 +32,7 @@ module Coursewareable
       if @response.save
         flash[:success] = _('Your response was added.')
         redirect_to lecture_assignment_response_path(
-          params[:lecture_id], @assignment, @response)
+          @lecture, @assignment, @response)
       else
         flash[:alert] = _('There was an error, please try again.')
         render :new
