1c96f9a4900805e61b2d0ecdc87c2797f24fd216
Calin Salagean <salagean.calin@gmail.com>
2013-02-26 16:40:12 +0200

    check settings when send email

--- app/mailers/grades_mailer.rb
+++ app/mailers/grades_mailer.rb
@@ -9,15 +9,23 @@ class GradesMailer < ActionMailer::Base
     @grade_user = grade.user
     @content = grade
     @date = Time.now
-    mail(:to => grade.receiver.email, 
-          :subject => "#{@grade_user.name} graded you!")
+    email_settings = @grade_user.memberships.find_by_classroom_id(
+      grade.classroom.id).email_announcement
+    if email_settings[:send_grades]
+      mail(:to => grade.receiver.email,
+            :subject => "#{@grade_user.name} graded you!")
+    end
   end
 
   def update_grade_email(grade)
     @grade_user = grade.user
     @content = grade
     @date = Time.now
-    mail(:to => grade.receiver.email, 
-          :subject => "#{@grade_user.name} modified your grade!")
+    email_settings = grade.receiver.memberships.find_by_classroom_id(
+    grade.classroom.id).email_announcement
+    if email_settings[:send_grades]
+      mail(:to => grade.receiver.email,
+            :subject => "#{@grade_user.name} modified your grade!")
+    end
   end
 end
