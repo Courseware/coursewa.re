e1b4ceaed11a9bc3aa0701f3c1e7fa111e9b6ac2
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-26 23:29:38 +0300

    Updated deploy script with ENV options.

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,7 @@ desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
     to :prepare do
-      invoke :stop
+      invoke :stop unless ENV['SKIP_STOP']
     end
 
     # Put things that will set up an empty directory into a fully set-up
@@ -77,7 +77,7 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
 
     to :launch do
-      invoke :start
+      invoke :start unless ENV['SKIP_START']
     end
   end
 end
