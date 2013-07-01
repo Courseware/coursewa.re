c1454649b20c21dac12ab9478f6499d4e624ec0c
Călin Sălăgean <salagean.calin@gmail.com>
2013-07-01 09:27:47 +0300

    remove token

--- config/initializers/14_stripe.rb
+++ config/initializers/14_stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :public_key => ENV['PUBLIc_KEY'] || 'pk_test_czwzkTp2tactuLOEOqbMTRzG',
-  :secret_key      => ENV['SECRET_KEY']
+  :public_key => ENV['PUBLIC_KEY'],
+  :secret_key => ENV['SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
